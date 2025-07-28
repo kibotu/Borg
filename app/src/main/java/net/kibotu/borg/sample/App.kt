b635d52f147b29f2f22276ff3c28a980ab337ba2
Jan Rabe <cloudgazer3d@gmail.com>
2025-07-28 09:18:08 +0200

    added app

---
+++ app/src/main/java/net/kibotu/borg/sample/App.kt
@@ -0,0 +1,10 @@
+package net.kibotu.borg.sample
+
+import android.app.Application
+import net.kibotu.borg.Borg
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
